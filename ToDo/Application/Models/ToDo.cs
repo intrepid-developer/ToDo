809cfc95ab799052614d13235117850dca6a5a0b
Chris <chris@intrepid-developer.com>
2023-01-09 21:00:30 +0000

    Upgrade to .Net 7 and moved ToDo class to new structure
    Upgraded the project to .Net 7
    Moved the ToDo class to new Application -> Models directory
    Fixed obsolete warning about Icons
    Updated Packages

---
+++ ToDo/Application/Models/ToDo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MudBlazor;
+
+namespace ToDo.Application.Models;
+
+public class ToDo
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Icon
+    {
+        get
+        {
+            if (IsComplete) return Icons.TwoTone.CheckBox;
+
+            return Icons.Material.Filled.CheckBoxOutlineBlank;
+        }
+    }
+
+    public bool IsComplete { get; set; }
+
+    public void SetComplete()
+    {
+        IsComplete = !IsComplete;
+    }
+}
